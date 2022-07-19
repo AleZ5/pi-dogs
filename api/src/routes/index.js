273101f22a28c0b44c1ce65de0d46354a044b1be
AleZ <ale.rom2552@gmail.com>
2022-07-18 23:25:44 -0500

    intento de db y back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,45 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require ( "axios");
+const Dog = require('../models/Dog');
+const Temperament = require('../models/Temperament');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getApiInfo = async () => {
+    const apiUrl = await axios.get ("https://api.TheDogAPI.com/v1/breeds?apikey=ed7fafcc-8586-4e70-ae2b-041f27d42c75");
+    const apiInfo = await apiUrl.data.map(el => {
+        return {
+            name: el.name,
+            id: el.dog_id,
+            height: el.height,
+            weight: el.weight,
+            years: el.years
+        };
+    });
+    return apiInfo;
+};
 
+const getDbInfo = async () => {
+    return await Dog.findAll ({
+        include:{
+            model: Temperament,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            },
+        }
+    })
+}
+
+const getAllDogs = async () =>{
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const infoTotal = apiInfo.concat(dbInfo);
+    return infoTotal
+}
 
 module.exports = router;
